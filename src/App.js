f6dc167b27e4e35b3afb418dd9c7132d356cbe0b
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-07-28 18:55:56 -0500

    Header and Nav components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
+  const [sections] = useState([
+    {
+      name: "About Me",
+    },
+    { name: "Portfolio" },
+    { name: "Contact Me" },
+    { name: "Resume" },
+  ]);
+
+  const [currentSection, setCurrentSection] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header
+        sections={sections}
+        currentSection={currentSection}
+        setCurrentSection={setCurrentSection}
+      ></Header>
+      <main></main>
     </div>
   );
 }
