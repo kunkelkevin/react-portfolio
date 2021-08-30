899a5228719ebdccd67887c0c5cd276fb15e0464
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-08-30 09:42:41 -0500

    Update index.js

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Project({ project }) {
   const { title, tech, description, link, gitHub, type } = project;
+  console.log(project,title);
   return (
     <section key={title}>
       <a
